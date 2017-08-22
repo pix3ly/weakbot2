d337d77b07ffa6356febf42132c1e6995e481423
Daniël van Drunen <daniel@pixely.me>
2017-08-22 15:17:44 +0200

    Add arguments check for lifts_add command

--- commands/lifts/add.js
+++ commands/lifts/add.js
@@ -1,13 +1,17 @@
 module.exports = (config, models, request, respond) => {
-    const user = request.from
-    const exercise = request.arguments[0]
-    const weight = request.arguments[1]
-    const unit = request.arguments[2]
-    const repetitions = request.arguments[3]
+    if (arguments[0] && arguments[1] && arguments[2] && arguments[3]) {
+        const user = request.from
+        const exercise = request.arguments[0]
+        const weight = request.arguments[1]
+        const unit = request.arguments[2]
+        const repetitions = request.arguments[3]
 
-    models['lift'].create({
-        user, exercise, weight, unit, repetitions
-    })
+        models['lift'].create({
+            user, exercise, weight, unit, repetitions
+        })
 
-    respond(request.from + ': done')
+        respond(request.from + ': done')
+    } else {
+        respond(request.from + ': wrong syntax, check out .help')
+    }
 }
