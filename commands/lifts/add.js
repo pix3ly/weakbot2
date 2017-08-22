83d2e8b5ca776a81360c46b5f69cd61fe7bd727e
Daniël van Drunen <daniel@pixely.me>
2017-08-22 15:38:48 +0200

    Add syntax check to lifts_add command

--- commands/lifts/add.js
+++ commands/lifts/add.js
@@ -1,17 +1,31 @@
 module.exports = (config, models, request, respond) => {
-    if (arguments[0] && arguments[1] && arguments[2] && arguments[3]) {
+    if (request.arguments.length === 4) {
         const user = request.from
         const exercise = request.arguments[0]
         const weight = request.arguments[1]
         const unit = request.arguments[2]
         const repetitions = request.arguments[3]
 
-        models['lift'].create({
-            user, exercise, weight, unit, repetitions
+        models['lift'].findOne({
+            where: {
+                user, exercise
+            }
+        }).then((lift) => {
+            if (lift) {
+                lift.weight = weight
+                lift.unit = unit
+                lift.repetitions = repetitions
+
+                lift.save()
+            } else {
+                models['lift'].create({
+                    user, exercise, weight, unit, repetitions
+                })
+            }
         })
 
         respond(request.from + ': done')
     } else {
-        respond(request.from + ': wrong syntax, check out .help')
+        respond(request.from + ': syntax error, check out .help')
     }
 }
